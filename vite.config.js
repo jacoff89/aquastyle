09b165d178155d977211b0923bab6611baaf7744
yakov <jacoff8920@mail.ru>
2026-03-26 15:59:18 +0300

    Обновил зависимости и php

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
     server: {
         host: '0.0.0.0',
-        port: 5173,
+        port: 5174,
         strictPort: true,
         hmr: {
             host: 'localhost'
